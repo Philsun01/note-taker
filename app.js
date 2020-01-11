aff0c1202a79c1476d7ccf75efd1101be4a22689
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-01-11 11:56:52 -0800

    generated note cards

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ console.log(userContainer);
 
 const API = 'https://acme-users-api-rev.herokuapp.com/api';
 
+const getNewUser = () => {
+    const newUserButton = document.querySelector('#newUser');
+    newUserButton.addEventListener('click',() => {
+        window.localStorage.removeItem('userId');
+        startApp();
+    })
+}
+
 const fetchUser = async ()=> {
   const storage = window.localStorage;
   const userId = storage.getItem('userId'); 
@@ -21,22 +29,43 @@ const fetchUser = async ()=> {
 };
 
 const loadUser = (user) => {
+    console.log(user);
     const card = document.querySelector("#user-info");
-    console.log(card);
-    console.log(user.fullName);
-    console.log(user.bio);
 
     const html = `<h2>${user.fullName}</h2>
-                    <img class = 'avatar' src = "chewbacca.jpeg">
+                    <img class = 'avatar' src = ${user.avatar}>
                     <p>${user.bio}</p>`;
 
     card.innerHTML = html;
 }
+const postNotes = async (id, note) => {
+    const res = axios.post(`${API}/users/${id}/notes`);
+}
+
+const getNotes = async (id) => {
+    const notes = (await axios.get(`${API}/users/${id}/notes`)).data;
+    const noteCard = document.querySelector('#notes');
+    noteCard.innerHTML = ``;
+    
+    if(notes) {
+        console.log(notes);
+        let html = `<h2> Notes (${notes.length}) </h2>`;
+        html = html + notes.map( note => {
+            return `<div class = 'note'> 
+                    <p>${note.text}</p>
+                    <button>X</button>
+                    </div>`
+        }).join('');
+        noteCard.innerHTML = html;
+
+    }
+    
+}
 
 const startApp = async() => {
     const user = await fetchUser();
-    
     loadUser(user);
+    getNotes(user.id);
 }
 
 startApp();
